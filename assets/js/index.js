338548ce678bfe62c331928a74fd3dc7319ab449
ElyesMaamouri <88774647+ElyesMaamouri@users.noreply.github.com>
2025-06-17 09:43:54 +0100

    Adjust card width in Services section

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,3 +84,13 @@ menuCloseBtn.onclick = closeMenu;
     video.pause();
     video.currentTime = 0;
   }
+
+
+   var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
